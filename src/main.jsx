5163c1a6c9d377a19e04486adcb3b31340c1bdd9
Velarken <181585418+Velarken@users.noreply.github.com>
2025-10-04 08:45:02 -0400

    removed unnecessary imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import GeneralInfo from './components/GeneralInfo.jsx'
-import Job from './components/Job.jsx'
-import Form from './components/Form.jsx'
-import FormField from './components/FormField.jsx'
+import './styles/index.css'
+import App from './components/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
