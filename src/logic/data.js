08a6adc1318c0e101cb44a2bb59c5d349a3da558
Velarken <181585418+Velarken@users.noreply.github.com>
2025-10-04 14:23:08 -0400

    created data objects for UI
    Each UI section is an array with an object per field

---
+++ src/logic/data.js
@@ -0,0 +1,36 @@
+export const CONTACTFIELDS = [
+    {type:'text',name:'firstName',label:'First Name:  '},
+    {type:'text',name:'middleInitial',label:'Middle Initial:  '},
+    {type:'text',name:'lastName',label:'Last Name:  '},
+    {type:'text',name:'emailAddress',label:'Email Address:  '},
+    {type:'tel',name:'phoneNumber',label:'Phone Number:  '},
+    {type:'text',name:'streetAddress',label:'Street Address:  '},
+    {type:'text',name:'city',label:'City / Township:  '},
+    {type:'text',name:'state',label:'State / Province:  '},
+    {type:'number',name:'zipCode',label:'Zip Code:  '}
+]
+export const JOBHISTORYFIELDS = [
+    {type:'text',name:'businessName',label:'Business Name:  '},
+    {type:'text',name:'jobTitle',label:'Job Title:  '},
+    {type:'text',name:'startMonth',label:'Start Month:  '},
+    {type:'number',name:'startYear',label:'Start Year:  '},
+    {type:'text',name:'endMonth',label:'End Month:  '},
+    {type:'number',name:'endYear',label:'End Year:  '},
+    {type:'textarea',name:'jobDescription',label:'Job Description:  '},
+    {type:'button',name:'addSkill',label:'Add Skill:  '}
+]
+export const EDUCATIONFIELDS = [
+    {type:'text',name:'schoolName',label:'School Name:  '},
+    {type:'text',name:'educationLevel',label:'Education Level:  '},
+    {type:'number',name:'yearStarted',label:'Started In:  '},
+    {type:'number',name:'yearFinished',label:'Ended In:  '},
+    {type:'checkbox',name:'didGraduate',label:'Graduated?  '},
+    {type:'text',name:'ciriculum',label:'Course of Study'},
+    {type:'textarea',name:'ciriculumDetails',label:'Describe Your Major:  '},
+]
+export const SKILLFIELDS = [
+    {type:'text',name:'jobSkill',label:'Skill:  '},
+    {type:'text',name:'proficiency',label:'Experience Length:  '}
+]
+
+// {type:'',name:'',label:''},
